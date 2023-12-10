1dd65d2e40b9f6ab747e173f2ef5c21953271161
Mohit Yadav <94161858+mohityadav1234@users.noreply.github.com>
2023-12-11 00:20:04 +0530

    Add files via upload

---
+++ Question5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Question5 {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      System.out.println("put n =");
+       int n = scanner.nextInt();
+
+       double sum = 0.0;
+        
+        for (int i = 1; i <= n; ++i) {
+            sum += 1.0/(i *i);
+        }
+        System.out.println("sum of series=" +sum);
+    
+        
+
+    }
+}
+   
